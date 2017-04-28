9233ed07d3c9724893165bf775a6ddf11c65f7fd
Irene <irene.penuelas445@west-mec.org>
2017-04-28 15:25:44 -0700

    All files uploaded. Jigsaw align images works
    Still needs some work but you can drag the images

---
+++ jpuzzle.js
@@ -0,0 +1,245 @@
+/*
+   Javascript: Exercise 6
+
+   Author: Irene J. Penuelas
+   Date: 4.26.17
+
+   Filename: jpuzzle.js
+
+
+   Global Variables List;
+
+   grids
+      An array of elements belonging to the grid class
+
+   pieces
+      An array of elements belonging to the pieces class
+
+   mousePiece
+      The puzzle piece currently selected by the user's mouse
+
+   keyPiece
+      The puzzle piece currently selected by the user's keyboard
+
+   keyIndex
+      The index number of keyPiece
+
+   selectMode
+      A Boolean value where true = keyboard use is in Select Piece mode,
+      false = keyboard use is in Move Piece mode
+
+   diffX
+      The horizontal distance in pixels between the left edge of
+      mousePiece and the mouse pointer
+
+   diffX
+      The vertical distance in pixels between the top edge of
+      mousePiece and the mouse pointer
+
+   maxZ
+      An integer representing the highest z-index value on the Web page
+
+   hoverGrid
+      The grid square that the moving puzzle piece is currently hovered over
+
+
+
+   Functions List:
+
+   jumbleIt()
+      Reloads the current Web page, thus re-arranging the puzzle
+
+   solveIt()
+      Places the puzzle images in the current order as background images
+      for the grid squares
+
+   init()
+      Sets up and initializes the Web page, defining the grid and pieces array,
+      and appying event handlers to mouse and keyboard actions
+
+   keyGrab(e)
+      "Grabs" a puzzle piece using the keyboard. Sets the value of the keyPiece
+      variable and creates an event object to handle keyboard events.
+      Pressing a spacebar toggles the operating mode between Select Piece and
+      Move Piece. Pressing the four arrow keys either selects a new piece or moves
+      the current piece
+
+   keyMove(moveX, moveY)
+      Moves the keyPiece moveX pixels to the right and moveY pixels down.
+
+   selectPiece(diffIndex)
+      Changes the index of the selected keyPiece by a value of diffIndex sets the
+      format of the new keyPiece
+
+   toggleMode()
+      Switches the Web page between Select Piece mode and Move Piece mode. Drops the
+      currently moving keyPiece onto the Web page, aligning it with the grid
+
+   dropValid(object)
+      Returns a Boolean value indicating whether it valid to drop the object. The function
+      is false if dropping object will cover a puzzle piece and true if otherwise
+
+   alignPiece(object)
+      If object is over a grid square, aligns object with the top-left corner of the square
+
+   highlightGrid(object)
+      If object is over a grid square, sets the background color of the square to light green
+
+   mouseGrab(e)
+      "Grabs" a puzzle piece using the mousedown action. Sets the value of mousePiece. Calculates
+      the value of diffX and diffY. Applies event handlers for mousemove and mouseup events
+
+   mouseMove(e)
+      Move mousePiece across the Web page, keeping a constant distance from the mouse pointer
+
+   mouseDrop(e)
+      Drops mousePiece on the Web page (if it is not over another puzzle piece). Aligns the
+      piece with the grid. Turns off event handlers for the mousemove and mouseup events.
+
+
+
+*/
+
+var grids = new Array();
+var pieces = new Array();
+var mousePiece = null;
+
+var diffX = null;  // Difference in X
+var diffY = null;  // Difference in Y
+var maxZ = 1;
+
+window.onload = init;
+
+function jumbleIt() {
+  // Reloads the current Web page, thus re-arranging the puzzle
+   window.location.reload();
+}
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~function solveIt~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+function solveIt() {
+  // Places the puzzle images in the current order as background images for the grid squares
+
+   for (var i = 0; i < grids.length; i++) {
+      pieces[i].style.backgroundImage = "";
+      grids[i].style.backgroundImage = "url(piece"+i+".jpg)";
+   }
+}
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~function init~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+function init() {
+  // Sets up and initializes the Web page, defining the grid and pieces array, and appying event handlers to mouse and keyboard actions
+
+   var allElem = document.getElementsByTagName("*");
+
+   for (var i = 0; i < allElem.length; i++) {
+      if (allElem[i].className == "grid") grids.push(allElem[i]); // push is adding elements to the end of the array
+      if (allElem[i].className == "pieces") pieces.push(allElem[i]);
+   }
+
+   var randomIntegers = randomArray(pieces.length);
+
+   for (var i = 0; i < pieces.length; i++) {
+     pieces[i].style.backgroundImage = "url(piece" + randomIntegers[i] + ".jpg)";
+     pieces[i].style.top = getStyle(pieces[i], "top");
+     pieces[i].style.left = getStyle(pieces[i], "left");
+     pieces[i].style.width = getStyle(pieces[i], "width");
+     pieces[i].style.height = getStyle(pieces[i], "height");
+
+     addEvent(pieces[i], "mousedown", mouseGrab, false);
+   }
+
+   for (var i = 0; i < grids.length; i++) {
+     grids[i].style.top = getStyle(grids[i], "top");
+     grids[i].style.left = getStyle(grids[i], "left");
+     grids[i].style.width = getStyle(grids[i], "width");
+     grids[i].style.height = getStyle(grids[i], "height");
+   }
+
+}
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~function dropValid~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+function dropValid(object) {
+  // Returns a Boolean value indicating whether it valid to drop the object. The function is false if dropping object will cover a puzzle piece and true if otherwise
+  for (var i = 0; i < pieces.length; i++) {
+    if (withinIt(object, pieces[i])){
+      return false;
+    }
+    return true;
+  }
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~function alignPiece~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+function alignPiece(object) {
+  for (var i = 0; i < grids.length; i++) {
+    if (withinIt(object, grids[i])) {
+      object.style.left = grids[i].style.left;
+      object.style.top = grids[i].style.top;
+      break;
+    }
+  }
+}
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~function mouseGrab~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+function mouseGrab(e) {
+  // "Grabs" a puzzle piece using the mousedown action. Sets the value of mousePiece. Calculates the value of diffX and diffY. Applies event handlers for mousemove and mouseup events
+
+  var evt = e || window.event; // First option means we're in W3C, second means IE
+  mousePiece = evt.target || evt.srcElement;
+
+  maxZ++;
+  mousePiece.style.zIndex = maxZ;
+
+  var mouseX = evt.clientX;  // x-coordinate of pointer
+  var mouseY = evt.clientY;  // Y-coordinate of pointer
+
+  // This calculates the distance from the pointer to the piece edge
+  diffX = parseInt(mousePiece.style.left) - mouseX;  // function but not a method. Not owned
+  diffY = parseInt(mousePiece.style.top) - mouseY;
+
+  addEvent(document, "mousemove", mouseMove, false);
+  addEvent(document, "mouseup", mouseDrop, false);
+
+  //alert("Event : " + evt.type + " on " + mousePiece.id);
+}
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~function mouseMove~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+function mouseMove(e) {
+  // Move mousePiece across the Web page, keeping a constant distance from the mouse pointer
+  var evt = e || window.event;
+  var mouseX = evt.clientX;
+  var mouseY = evt.clientY;
+
+  mousePiece.style.left = mouseX + diffX + "px";
+  mousePiece.style.top = mouseY + diffY + "px";
+}
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~function mouseDrop~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+function mouseDrop(e) {
+  // Drops mousePiece on the Web page (if it is not over another puzzle piece). Aligns the piece with the grid. Turns off event handlers for the mousemove and mouseup events.
+
+  if (dropValid(mousePiece)){
+    alignPiece(mousePiece);
+    removeEvent(document, "mousemove", mouseMove, false);
+    removeEvent(document, "mouseup", mouseDrop, false);
+  }
+}
